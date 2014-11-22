2c878e9a02a1dc6852e7d6cb17e91628f212ecbe
evlizashel <evlizashel@gmail.com>
2014-11-22 22:35:31 +0300

    Programming Assignment2
    Solving and caching matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Hi! 
+## Here is my task, hope you like the way I did it, though there were no really big space to be creative :)
+## Please, don't be strict :)
+##   NOTE: Firstly I have started to write code for 
+## setting the matrix, not for using existing one. 
+## when you try my function,
+## set it like "m<- makeCacheMatrix(1:7,6,6)
+## where 6s are number of cols and rows or "m$set(1:6,5,5)"
+## I understand, that we may need input a given matrix, -
+## - this way we just change in code "x,a,b" to "x = matrix()", 
+## the same for "y,k,l" to "y"
+## Thank you for understanding! 
+## - Eva
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x,a,b) {
+	  x <- matrix(x,a,b)	## input the matrix with number of rows and cols
+        i <- NULL
+        set <- function(y, k, l) { ## Set matrix with number of rows and cols
+                x <<- matrix(y, k, l) 
+                i <<- NULL
+        }
+        get <- function() x  ## Set "data"
+        setsolve <- function(solve) i <<- solve ## set the calculated value
+        getsolve <- function() i  ## calculates
+        list(set = set, get = get,     ## create kind of object, where stores all we need for calculation
+             setsolve = setsolve,
+             getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## I am happy it works!
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      i <- x$getsolve()                   ## checking for cached inversion
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()      ## if no cached invertion, loads data
+        i <- solve(data)     ## calculates inversion
+        x$setsolve(i)        ## set calculated value into our object
+        i
 }
